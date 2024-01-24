f37667bbc86b00360ee36c1bfd504dc51c176781
Spudstak <64370374+Spudstak@users.noreply.github.com>
2024-01-23 20:45:33 -0400

    Added CLI, partial test cases (requires other functionaility before they can be fully functional), and sample data.

---
+++ testLMHInterface.py
@@ -0,0 +1,32 @@
+import unittest
+import subprocess
+import os
+
+class TestLMHInterface(unittest.TestCase):
+
+    def setUp(self):
+        print('\nsetUp')
+
+    def tearDown(self):
+        print('\ntearDown')
+
+    def test_basic_functionality_isbn(self):
+        result = subprocess.run(['python', 'lmh.py', '-i', 'test_data/metadata_sample_isbns.txt', '-o', 'output.tsv', '--retrieve-ocns'], capture_output=True, text=True)
+        self.assertEqual(result.returncode, 0)
+        # self.assertTrue("Input data: ['9780191513015', '9780191513022']" in result.stdout)
+        # self.assertTrue("Retrieved OCNs: ['000000000', '000000000']" in result.stdout)
+
+    def test_basic_functionality_ocn(self):
+        result = subprocess.run(['python', 'lmh.py', '-i', 'test_data/sample_metadata_ocns.txt', '-o', 'output.tsv', '--retrieve-isbns'], capture_output=True, text=True)
+        self.assertEqual(result.returncode, 0)
+        #self.assertTrue("Input data: ['922903415', '922903416']" in result.stdout)
+        # self.assertTrue("Retrieved ISBNs: ['0000000000000', '0000000000000']" in result.stdout)
+
+    def test_output_file_handling(self):
+        result = subprocess.run(['python', 'lmh.py', '-i', 'test_data/metadata_sample_isbns.txt', '--retrieve-ocns'], capture_output=True, text=True)
+        self.assertEqual(result.returncode, 0)
+        # self.assertTrue("Retrieved OCNs: ['000000000', '000000000']" in result.stdout)
+        # self.assertTrue(os.path.exists('output.tsv'))  # Check if output file is generated
+
+if __name__ == '__main__':
+    unittest.main()
