ac2d7561e629c2b9a7567c98c3cb469487f0c801
sinaf <sinafathi.cs@gmail.com>
2024-01-30 20:34:04 -0400

    removed features that were already in the input system

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import json
 
-
+"""
+This code will be updated at later point with any necessary new implementations
+current features {
+timeout time for a single ISBN search
+} 
+"""
 class Config:
     def __init__(self, config_file='config.json'):
         self.config_file = config_file
@@ -27,40 +32,13 @@ class Config:
         with open(self.config_file, 'w') as f:
             json.dump(new_config, f, indent=4)
 
-    def set_sources(self, sources):
-        self.config["sources"] = sources
-        self.save_config(self.config)
-
-    def set_priority_order(self, priority_order):
-        self.config["priority_order"] = priority_order
-        self.save_config(self.config)
-
-    def set_include_LCCN(self, include_LCCN):
-        self.config["include_LCCN"] = include_LCCN
-        self.save_config(self.config)
-
-    def set_include_DOI(self, include_DOI):
-        self.config["include_DOI"] = include_DOI
-        self.save_config(self.config)
-
-    def set_OCLC_API_key(self, OCLC_API_key):
-        self.config["OCLC_API_key"] = OCLC_API_key
-        self.save_config(self.config)
-
     def set_search_timeout(self, search_timeout):
         self.config["search_timeout"] = search_timeout
         self.save_config(self.config)
 
 
-# Example usage:
-# Later down the line other branches can use this config to set up the settings
 if __name__ == "__main__":
     config = Config()
 
     # Set new configuration options
-    config.set_sources(["Library of Congress", "WorldCat", "British Library"])
-    config.set_priority_order(["WorldCat", "Library of Congress"])
-    config.set_include_LCCN(False)
-    config.set_include_DOI(False)
-    config.set_OCLC_API_key("NEW_OCLC_API_KEY")
     config.set_search_timeout(60)
