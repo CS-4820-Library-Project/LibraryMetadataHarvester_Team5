722ee71af9b9a8d150183b0f6ad92b0529423bfc
shiva2321 <54901808+shiva2321@users.noreply.github.com>
2024-02-13 17:18:15 -0400

    Create notificationSys.py
    notification reference class

---
+++ notificationSys.py
@@ -0,0 +1,12 @@
+class NotificationSystem:
+    def __init__(self):
+        pass
+
+    def notify_user(self, message):
+        """
+        Notify the user about an error or informational message.
+
+        Parameters:
+        - message (str): The message to be displayed to the user.
+        """
+        print(f"Notification: {message}")
